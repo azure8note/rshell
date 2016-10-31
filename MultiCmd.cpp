ffff8ca6da6cd8f2bb0ab15d42b97816354f1d1b
azure8note <j.shing.lin@gmail.com>
2016-10-31 15:21:27 -0700

    Updated the execute

--- MultiCmd.cpp
+++ MultiCmd.cpp
@@ -23,5 +23,42 @@ bool MultiCmd::getStatus() {
 }
 
 void MultiCmd::execute() {
-	//FIXME NEEDS TO BE DONE
+
+	char* cmd;
+	cmd = strtok(cmdString.c_str(), " ");
+	
+	while (cmd != NULL) {
+
+		if(cmd[0] == "#"){ //comment case
+			return;
+		}
+		else if( *cmd == "&&" && cmdStatus){// && case
+			if (execvp(/*PATH "/bin/" ?*/, cmd, NULL) < 0){//? idk if this works
+				cmdStatus = false;
+				out << "ERROR EXECUTING" << endl;
+			}
+			else {
+				cmdStatus = true;
+				cmd = strtok(NULL, " ");
+			}
+		}
+		else if ( *cmd == "||" && !cmdStatus){ // || case
+			if (execvp(/*PATH "/bin/" ?*/, cmd, NULL) < 0){//? idk if this works
+				cmdStatus = false;
+				out << "ERROR EXECUTING" << endl;
+			}
+			else {
+				cmdStatus = true;
+				cmd = strtok(NULL, " ");
+			}
+		}
+		else if (execvp(/*PATH "/bin/" ?*/, cmd, NULL) < 0){//? idk if this works //general case
+			cmdStatus = false;
+			cout << "ERROR EXECUTING" << endl;
+		}
+		else {
+				cmdStatus = true;
+				cmd = strtok(NULL, " ");
+		}
+	}
 }
