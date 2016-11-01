9638bccec172b85f8cef104a66108bf8b14b96e9
azure8note <j.shing.lin@gmail.com>
2016-10-31 23:47:33 -0700

    Still need to resolve string vs Cstring issues

--- CmdComposite.cpp
+++ CmdComposite.cpp
@@ -5,9 +5,9 @@
 void CmdComposite::addCmd(char* cmd) {
 	if (cmd->find("&&") != string::npos || cmd->find("||") != string::npos)
 	{
-		cmdList.push_back(new SingleCmd(*cmd));
+		cmdList.push_back(new SingleCmd(cmd));
 	} else {
-		cmdList.push_back(new MultiCmd(*cmd));
+		cmdList.push_back(new MultiCmd(cmd));
 	}
 
 	return;
@@ -16,14 +16,16 @@ void CmdComposite::addCmd(char* cmd) {
 void CmdComposite::parse(int argc, char* argv[]) {
 	//string cmdLine = readCmd();
 	string cmdLine = "";
+	
 	char* tokptr; //Token ptr
 
 	for (int i = 0; i < argc; ++i) {
 		cmdLine += argv[i];
-	}	
+	}
+	char* cmdLinePtr = cmdLine.c_str();
 	
 	// Set tokptr to beginning of first token
-	tokptr = strtok(cmdLine, ";"); // Tokens split by ; can be single or multi commands
+	tokptr = strtok(cmdLinePtr, ";"); // Tokens split by ; can be single or multi commands
 	while (tokptr != NULL) {
 		addCmd(tokptr);
 		tokptr = strtok(NULL, ";"); // Continue scanning where prev successful call ended
