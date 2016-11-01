81f86efff8c6f28c9b711c9c8a3c4adbd2a74b43
anchiou <anniechiou6@yahoo.com>
2016-11-01 11:10:56 -0700

    Resolved merge conflict

--- Base.h
+++ Base.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef BASE_H
 #define BASE_H
 
@@ -12,18 +11,3 @@ protected:
 
 };
 #endif
-=======
-#ifndef BASE_H
-#define BASE_H
-
-class Base {
-
-  public:
-	//Constructor
-	Base() {};
-	//Pure virtual function
-	virtual void execute() = 0;
-
-};
-#endif
->>>>>>> 0b0078a71ef0901184db23eca55d8582266560f1
