f59626982e427708114c0d6714efe795bbebd697
anchiou <anniechiou6@yahoo.com>
2016-11-01 11:53:50 -0700

    Fixed errors dealing with string to c_string conversion

--- CmdComposite.cpp
+++ CmdComposite.cpp
@@ -3,8 +3,9 @@
 #include "MultiCmd.h"
 
 void CmdComposite::addCmd(char* cmd) {
-	if (cmd->find("&&") != string::npos || cmd->find("||") != string::npos)
-	{
+	string cmdCpy(cmd);
+	if (cmdCpy.find("&&") != string::npos ||
+	    cmdCpy.find("||") != string::npos) {
 		cmdList.push_back(new SingleCmd(cmd));
 	} else {
 		cmdList.push_back(new MultiCmd(cmd));
@@ -16,22 +17,21 @@ void CmdComposite::addCmd(char* cmd) {
 void CmdComposite::parse(int argc, char* argv[]) {
 	//string cmdLine = readCmd();
 	string cmdLine = "";
-	
-	char* tokptr; //Token ptr
 
 	for (int i = 0; i < argc; ++i) {
 		cmdLine += argv[i];
 	}	
 
-	char* cmdLinePtr = cmdLine.c_str();
+	char* cstr = new char [cmdLine.length() + 1];
+	strcpy(cstr, cmdLine.c_str()); // cstr now contain c-string copy of cmdLine
 	
 	// Set tokptr to beginning of first token
-	tokptr = strtok(cmdLinePtr, ";"); // Tokens split by ; can be single or multi commands
+	char* tokptr = strtok(cstr, ";"); // Tokens split by ; can be single or multi commands
 	while (tokptr != NULL) {
 		addCmd(tokptr);
 		tokptr = strtok(NULL, ";"); // Continue scanning where prev successful call ended
 	}
-
+	delete[] cstr; // De-allocate cstr memory
 	return;
 }
 
