41fccd7f67e2f6b7e3136d7cc5023f14ec22ae86
azure8note <j.shing.lin@gmail.com>
2016-11-01 14:21:15 -0700

    no changes

--- CmdComposite.h
+++ CmdComposite.h
@@ -6,6 +6,7 @@
 #include <cstring>
 #include <iostream>
 
+
 using std::vector;
 using std::string;
 
--- MultiCmd.h
+++ MultiCmd.h
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 using std::string;
 using std::cout;
