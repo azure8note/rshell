e9300fc619f88b8c8a35d44e68ad489df1329981
azure8note <j.shing.lin@gmail.com>
2016-11-02 02:55:28 -0700

    Changed Execute() added functions

--- MultiCmd.cpp
+++ MultiCmd.cpp
@@ -1,40 +1,99 @@
 #include "MultiCmd.h"
 
-void MultiCmd::changeCmd(char* str){
+void MultiCmd::setCmd(char* str){
 	cmdString = str;
 	return;
 }
 
-char* MultiCmd::readCmd() {
+char* MultiCmd::getCmd() {
 	return cmdString;
 }
 
-void MultiCmd::changeStatus() {
-	if (cmdStatus){
-		cmdStatus = false;
-	} 
-	else {
-		cmdStatus = true;
-	}
+void MultiCmd::setStatus(bool b) {
+	cmdStatus = b;
 }
 
 bool MultiCmd::getStatus() {
 	return cmdStatus;
 }
 
-void MultiCmd::execute() {
+void MultiCmd::parse() {
   
-      string an = "&&";
-      string o = "||";
-  
-     if(cmdString[0] == ' '){
+  if(cmdString[0] == ' '){// removes space in front of ;
 	string cmdCpy(cmdString);
 	cmdCpy = cmdCpy.substr(1, cmdCpy.length() - 1);
 	char* cstr = new char[cmdCpy.length()+1];
 	strcpy(cstr, cmdCpy.c_str());
 	cmdString = cstr;
-     }
+  }
   
+  char* tok;
+  
+  tok = strtok(cmdString, ' ');
+  Cmds.push_back(tok);
+  
+  for(unsigned i = 0; tok != NULL; i++){//changes string into vector of tokens
+    tok = strtok(NULL, ' ');
+    Cmds.push_back(tok);
+  }
+}
+
+void MultiCmd::makeQueue() {
+  
+  string an = "&&";
+  string o = "||";
+  
+  Base* temp = 0;
+  
+  for(unsigned i = 0; i < reversedCmds.size(); i++){
+     if (Cmds.at(i) == an.c_str()){
+       temp = new And();
+       cmdQueue.push_back(temp);
+     }
+     else if (Cmds.at(i) == o.c_str()){
+       temp = new Or();
+       cmdQueue.push_back(temp);
+     }
+     else {
+       temp = new SingleCmd(Cmds.at(i));
+       cmdQueue.push_back(temp);
+     }
+  }e
+
+void MultiCmd::execute() {
+  
+  this->parse();
+  this->makeQueue();
+  
+  Base* leftPtr;
+  Base* midPtr;
+  Base* rightPtr;
+  Base* dum = new SingleCmd();
+  
+  leftPtr = cmdQueue.front();
+  cmdQueue.pop_front();
+  midPtr = cmdQueue.front();
+  cmdQueue.pop_front();
+  rightPtr = cmdQueue.front();
+  cmdQueue.pop_front();
+  
+  midPtr->setLeft(leftPtr);
+  midPtr->setRight(rightPtr);
+  
+  midPtr->execute();
+  
+  while (!cmdQueue.empty()){
+    dum->setStatus(midPtr->getStatus());
+    midPtr = cmdQueue.front();
+    cmdQueue.pop_front();
+    rightPtr = cmdQueue.front();
+    cmdQueue.pop_front();
+    midPtr->execute();
+  }
+}
+  
+      
+ /*
 	char* tok;
 	tok = strtok(cmdString, " ");
 	char* args[64];
@@ -48,7 +107,7 @@ void MultiCmd::execute() {
 		args[0] = tok;
 		args[1] = NULL;
 		
-		pid = fork();/* forks child process */
+		pid = fork();/* forks child process
 		
 		if (pid == -1) {
 		      perror("fork");
@@ -96,4 +155,4 @@ void MultiCmd::execute() {
 		  while(!WIFEXITED(status) && !WIFSIGNALED(status));
 		}
 	}
-}
+	*/
