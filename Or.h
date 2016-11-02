d661630967423348664370df8292cf201c48ae3d
anchiou <anniechiou6@yahoo.com>
2016-11-02 03:18:37 -0700

    Added cmdStatus

--- Or.h
+++ Or.h
@@ -2,24 +2,24 @@
 #define OR_H
 
 #include "Base.h"
-#include <stddef>
+#include <stddef.h>
 
 class Or : public Base {
 	private:
 		Base* leftCmd;
 		Base* rightCmd;
 		bool cmdStatus;
-		
+
 	public:
 		// Constructor
-		Or() : Base(), leftCmd(NULL), rightCmd(NULL) {};
+		Or() : Base(), leftCmd(NULL), rightCmd(NULL), cmdStatus(false) {};
 
-		// Accessors for left, right command ptrs, and cmdStatus
+		// Accessors for left, right command ptrs and cmdStatus
 		Base* getLeftCmd() const;
 		Base* getRightCmd() const;
 		bool getCmdStatus() const;
-		
-		// Mutators for leftCmd, rightCmd, and cmdStatus
+
+		// Mutators for leftCmd, rightCmd, and cmdStatus 
 		void setLeftCmd(Base* leftCmd);
 		void setRightCmd(Base* rightCmd);
 		void setCmdStatus(bool stat);
