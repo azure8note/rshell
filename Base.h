56788af62c5a7f01f28e8b288479206f6c8df63e
azure8note <j.shing.lin@gmail.com>
2016-11-02 04:32:20 -0700

    fixed stuff

--- Base.h
+++ Base.h
@@ -1,18 +1,20 @@
 #ifndef BASE_H
 #define BASE_H
 
+#include <stddef.h>
+
 class Base {
 public:
 	//Constructor
 	Base() {};
 	//Pure virtual function
 	virtual void execute() = 0;
-    virtual Base* getLeftCmd() const {};
-    virtual Base* getRightCmd() const {};
-    virtual bool getCmdStatus() const {};
+    virtual Base* getLeftCmd() const { return NULL;};
+    virtual Base* getRightCmd() const { return NULL;};
+    virtual bool getCmdStatus() const { return false;};
     virtual void setLeftCmd(Base*) {};
     virtual void setRightCmd(Base*) {};
-
+    virtual char* getCmd() const {return NULL;};
 };
 
 #endif
