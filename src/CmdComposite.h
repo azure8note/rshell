b2cc7fa77dc7f62f8b373e0e08bad8daf23b6315
anchiou <anniechiou6@yahoo.com>
2016-11-02 11:27:47 -0700

    Moved to src directory

---
+++ src/CmdComposite.h
@@ -0,0 +1,32 @@
+#ifndef CMDCOMP_H
+#define CMDCOMP_H
+#include "Base.h"
+
+#include <stddef.h>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+
+
+using std::vector;
+using std::string;
+
+class CmdComposite : public Base {
+private:
+	vector <Base*> cmdList;
+	
+	
+public:
+	//Constructor
+	CmdComposite() : Base() {};
+	
+	void parse(string cmdLine);
+	void addCmd(char* cmd);
+	void execute();
+
+	//Accessor
+	Base* getCmd(int a) const;
+};
+
+#endif
