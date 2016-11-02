bc842da8ce2ac9148a67086c5ea1c563100d23da
azure8note <j.shing.lin@gmail.com>
2016-11-02 04:04:35 -0700

    added more virtual functions

--- Base.h
+++ Base.h
@@ -7,6 +7,11 @@ public:
 	Base() {};
 	//Pure virtual function
 	virtual void execute() = 0;
+    virtual Base* getLeftCmd() const {};
+    virtual Base* getRightCmd() const {};
+    virtual bool getCmdStatus() const {};
+    virtual void setLeftCmd(Base*) {};
+    virtual void setRightCmd(Base*) {};
 
 };
 
