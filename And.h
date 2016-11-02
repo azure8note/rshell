d9606bf74c1f0120c89955ae6d4ce37e559b9baf
anchiou <anniechiou6@yahoo.com>
2016-11-02 02:59:54 -0700

    And header file

--- And.h
+++ And.h
@@ -8,19 +8,22 @@ class And : public Base {
 	private:
 		Base* leftCmd;
 		Base* rightCmd;
-		
+		bool cmdStatus;
+
 	public:
 		// Constructor
-		And() : Base(), leftCmd(NULL), rightCmd(NULL) {};
+		And() : Base(), leftCmd(NULL), rightCmd(NULL);
 
-		// Accessors for left and right command ptrs
-		Base* getLeftCmd();
-		Base* getRightCmd();
-		
-		// Mutators for left and right 
+		// Accessors for left, right command ptrs and cmdStatus
+		Base* getLeftCmd() const;
+		Base* getRightCmd() const;
+		bool getCmdStatus() const;
+
+		// Mutators for leftCmd, rightCmd, and cmdStatus 
 		void setLeftCmd(Base* leftCmd);
 		void setRightCmd(Base* rightCmd);
-		
+		void setCmdStatus(bool stat);
+
 		// Calls execute 
 		void execute();
 };
