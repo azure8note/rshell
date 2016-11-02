3cf5a3dec17be32aad70fe58d7b46b6a41c8ccbc
anchiou <anniechiou6@yahoo.com>
2016-11-02 11:29:08 -0700

    Moved to src directory

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include "Base.h"
+#include "SingleCmd.h"
+#include "MultiCmd.h"
+#include "CmdComposite.h"
+
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	//bool flag = true;
+	char hostname[256];
+	char* usrname = getlogin();
+	gethostname(hostname, sizeof hostname);
+    
+	cout << "\033c";
+	cout << usrname  << "@" << hostname << " $ ";
+	
+	string cmdLine = "";
+	CmdComposite* cmdList = new CmdComposite();
+	getline(cin, cmdLine);	
+	cmdList->parse(cmdLine);
+	cmdList->execute();
+	/*	
+	while() {
+		cout << usrname  << "@" << hostname << " $ ";
+		getline(cin, cmdLine);	
+		cmdList->parse(cmdLine);
+		cmdList->execute(); // exit command is checked for in execute()
+	}
+	*/  
+	return 0;
+}
