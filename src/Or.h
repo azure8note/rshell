d1d5848be10ed9f16ae599facc12776bf9d2da8a
anchiou <anniechiou6@yahoo.com>
2016-11-02 11:27:31 -0700

    Moved to src directory

---
+++ src/Or.h
@@ -0,0 +1,35 @@
+#ifndef OR_H
+#define OR_H
+
+#include "Base.h"
+#include <stddef.h>
+
+class Or : public Base {
+	private:
+		Base* leftCmd;
+		Base* rightCmd;
+		bool cmdStatus;
+
+	public:
+		// Constructor
+		Or() : Base() {
+		  leftCmd = NULL;
+		  rightCmd = NULL;
+		  cmdStatus = false;
+		};
+
+		// Accessors for left, right command ptrs and cmdStatus
+		Base* getLeftCmd() const;
+		Base* getRightCmd() const;
+		bool getCmdStatus() const;
+
+		// Mutators for leftCmd, rightCmd, and cmdStatus 
+		void setLeftCmd(Base* leftCmd);
+		void setRightCmd(Base* rightCmd);
+		void setCmdStatus(bool stat);
+
+		// Calls execute 
+		void execute();
+};
+
+#endif
