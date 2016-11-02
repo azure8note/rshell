107f7249538e5aed2b0a0dc4c99f633a8ba26b07
anchiou <anniechiou6@yahoo.com>
2016-11-02 11:27:27 -0700

    Moved to src directory

---
+++ src/And.h
@@ -0,0 +1,35 @@
+#ifndef AND_H
+#define AND_H
+
+#include "Base.h"
+#include <stddef.h>
+
+class And : public Base {
+	private:
+		Base* leftCmd;
+		Base* rightCmd;
+		bool cmdStatus;
+
+	public:
+		// Constructor
+		And() : Base() {
+		  leftCmd = NULL;
+		  rightCmd = NULL;
+		  cmdStatus = false;
+		};
+
+		// Accessors for left, right command ptrs and cmdStatus
+		Base* getLeftCmd() const;
+		Base* getRightCmd() const;
+		bool getCmdStatus() const;
+
+		// Mutators for leftCmd, rightCmd, and cmdStatus 
+		void setLeftCmd(Base* leftCmd);
+		void setRightCmd(Base* rightCmd);
+		void setCmdStatus(bool stat);
+
+		// Calls execute 
+		void execute();
+};
+
+#endif
