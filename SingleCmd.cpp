9a9c61ec4b9c19e8f8cadd57322eda171f4d4018
anchiou <anniechiou6@yahoo.com>
2016-11-02 05:34:22 -0700

    No change

--- SingleCmd.cpp
+++ SingleCmd.cpp
@@ -19,51 +19,52 @@ void SingleCmd::setStatus(bool b){
 
 void SingleCmd::execute() {
   
-  if(cmd[0] == ' '){
-     string cmdCpy(cmd);
-     cmdCpy = cmdCpy.substr(1, cmdCpy.length() - 1);
-     char* cstr = new char[cmdCpy.length()+1];
-     strcpy(cstr, cmdCpy.c_str());
-     cmd = cstr;
-  }
+	if(cmd[0] == ' '){
+		string cmdCpy(cmd);
+		cmdCpy = cmdCpy.substr(1, cmdCpy.length() - 1);
+		char* cstr = new char[cmdCpy.length()+1];
+		strcpy(cstr, cmdCpy.c_str());
+		cmd = cstr;
+	}
   
-  string lowE = "exit";
-  string capE = "EXIT";
+	string lowE = "exit";
+	string capE = "EXIT";
   
-  char* tok;
+	char* tok;
   
-  char* args[64];
+	char* args[64];
   
-  tok = strtok(cmd, " ");
-  args[0] = tok;
+	tok = strtok(cmd, " ");
+	args[0] = tok;
   
-  for (unsigned i = 1; tok != NULL ; i++){
-    tok = strtok(NULL, " ");
-    args[i] = tok;
-  }
+	for (unsigned i = 1; tok != NULL ; i++){
+		tok = strtok(NULL, " ");
+    		args[i] = tok;
+  	}	
   
-  if (tok == lowE.c_str() || tok == capE.c_str()){
-    exit(0);
-  }
+  	if (tok == lowE.c_str() || tok == capE.c_str()){
+    		exit(0);
+  	}
 
-  pid_t pid;
-  pid_t wpid;
-  int status;
+  	pid_t pid;
+  	int status;
   
 	pid = fork();
   
 	if (pid < 0) { /*fork a child process*/
-	    perror("fork");
+	   	 perror("fork");
 	} 
 	else if(pid == 0){ //child processs
-	  if (execvp(args[0], args) < 0){
-		setStatus(false);
-		perror ("exec");
-	  }
+	  	if (execvp(args[0], args) < 0){
+			setStatus(false);
+			perror ("exec");
+	  	}	
 	}
 	else {//parent
-	  do {
-	    wpid = waitpid(pid, &status, WUNTRACED);
-	  }
-	  while(!WIFEXITED(status) && !WIFSIGNALED(status));}
+  		pid_t wpid;
+	  	do {
+	    		wpid = waitpid(pid, &status, WUNTRACED);
+	  	}
+	  	while(!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
 }
