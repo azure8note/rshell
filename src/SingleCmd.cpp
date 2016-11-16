4787498a4db7f92e73b3bce903b5a3434b77d348
anchiou <anniechiou6@yahoo.com>
2016-11-15 20:20:53 -0800

    No change

--- src/SingleCmd.cpp
+++ src/SingleCmd.cpp
@@ -6,75 +6,70 @@ void SingleCmd::setCmd(char* str){
 }
 
 char* SingleCmd::getCmd() const{
-	return cmd;
+    return cmd;
 }
 
 bool SingleCmd::getCmdStatus() const{
-  return cmdStatus;
+    return cmdStatus;
 }
 
 void SingleCmd::setCmdStatus(bool b){
-  cmdStatus = b;
+    cmdStatus = b;
 }
 //----------------------------------
 
-void SingleCmd::parse() {// seperates the command from its flag into two seperate tokens
-  
-  char* tok;
-  
-	if(cmd[0] == ' '){ // if there is a space after the ';' this removes it
-		string cmdCpy(cmd);
-		cmdCpy = cmdCpy.substr(1, cmdCpy.length() - 1);
-		char* cstr = new char[cmdCpy.length()+1];
-		strcpy(cstr, cmdCpy.c_str());
-		cmd = cstr;
-	}
-  
+void SingleCmd::parse() { // Seperates the command from its flag into two seperate tokens  
+    char* tok;
 
-	tok = strtok(cmd, " "); //first seperation
-	args[0] = tok;
+    if (cmd[0] == ' '){ // If there is a space after the ';', this removes it.
+	string cmdCpy(cmd);
+	cmdCpy = cmdCpy.substr(1, cmdCpy.length() - 1);
+	char* cstr = new char[cmdCpy.length()+1];
+	strcpy(cstr, cmdCpy.c_str());
+	cmd = cstr;
+    }
   
-	for (unsigned i = 1; tok != NULL ; i++){ //assigns the tokens into args
-	  tok = strtok(NULL, " ");
-	  args[i] = tok;
+    tok = strtok(cmd, " "); // First seperation
+    args[0] = tok;
+
+    for (unsigned i = 1; tok != NULL ; i++){ // Assigns the tokens into args
+	tok = strtok(NULL, " ");
+	args[i] = tok;
     }
 }
 
 void SingleCmd::execute() {
-  
-  this->parse(); //makes the function readable
-  
-  string lowE = "exit";
-  string capE = "EXIT";
-
-  string argsCpy(args[0]);
+    this->parse(); // Further parses cmd into the actual command and any flags it has
+    
+    string argsCpy(args[0]);
+    
+    string lowExit = "exit";
+    string capExit = "EXIT";
  
-  if (argsCpy == lowE.c_str() || argsCpy == capE.c_str()) { // checks exit case
-    exit(0);
-  }
+    if (argsCpy == lowExit || argsCpy == capExit) { // Checks for command exit
+    	exit(0);
+    }  
 
-  	pid_t pid; //for the forking
-  	int status; //for the waiting
+    pid_t pid; // For the forking
+    int status; // For the waiting
   
-	pid = fork(); // fork fro child process
+    pid = fork(); // Fork from child process
   
-	if (pid < 0) { /*fork a child process*/
-	   	 perror("fork");// didn't fork properly
+    if (pid < 0) { // Fork a child process
+   	perror("fork"); // Did not fork properly
+    } 
+    else if(pid == 0){ // Child processs
+	if (execvp(args[0], args) < 0){ // Calls execute
+	    setCmdStatus(false); // If it fails, changes cmdStatus to false (used in MultiCmd)
+	    perror ("exec"); // Error checks
+	}	
+    } else { // Parent
+  	pid_t wpid; // Waits for child to finish
+	wpid = waitpid(pid, &status, WUNTRACED);
+	  	
+	while (wpid <= -1){
+	    perror("wait");
+	    wpid = waitpid(pid, &status, WUNTRACED);
 	} 
-	else if(pid == 0){ //child processs
-	  	if (execvp(args[0], args) < 0){// calls execute
-			setCmdStatus(false);//in case it fails changes the status (for multi comand
-			perror ("exec"); //error checks
-	  	}	
-	}
-	else {//parent
-  		pid_t wpid; // waits for child to finish
-	  	
-		wpid = waitpid(pid, &status, WUNTRACED);
-	  	
-		while (wpid <= -1){
-		  perror("wait");
-		  wpid = waitpid(pid, &status, WUNTRACED);
-		} 
-	}
+    }
 }
