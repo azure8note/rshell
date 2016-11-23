5f6b5e1995e66ca10806d55cd80def1a1e4cb3e4
anchiou <anniechiou6@yahoo.com>
2016-11-22 20:30:18 -0800

    Removed pwd and oldPwd variables

--- src/CdCmd.h
+++ src/CdCmd.h
@@ -6,12 +6,12 @@
 #include <string>
 #include <unistd.h> // chdir
 
-class CdCmd : public Base {
-	private:
-		string pwd;
-		string oldPwd;
-	
+class CdCmd : public Base {	
 	public:
+		/* Constructors */
+		CdCmd() : Base() {}
+		CdCmd(char* path) : Base() {}
+
 		/* Accessors and Mutators */
 		string getPWD() const;
 		string getOLDPWD() const;
